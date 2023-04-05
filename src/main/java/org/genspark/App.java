3efe529f18d71e93a0d393f5c3f4e0dd4b1756ff
Zachary Perales <zachary.perales@genspark.net>
2023-04-04 21:04:43 -0400

    Create and initialize driver class for the application.
    Create ApplicationContext instance, cast the student bean from the context to a Student object and print the student's information to the console.
    
    Closes #1

--- src/main/java/org/genspark/App.java
+++ src/main/java/org/genspark/App.java
@@ -1,13 +1,17 @@
 package org.genspark;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        // Creates an ApplicationContext instance using Spring's ClassPathXmlApplicationContext
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+
+        // Retrieves a bean named "student" from the context and casts it to a Student object
+        Student student = (Student) context.getBean("student");
+
+        // Prints the student's information to the console
+        System.out.println(student);
     }
 }
