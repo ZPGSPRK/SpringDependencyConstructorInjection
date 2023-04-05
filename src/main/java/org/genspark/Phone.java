b8cdcd67ca0dfba2b8cef08dcb609bd4173cc9c4
Zachary Perales <zachary.perales@genspark.net>
2023-04-04 20:37:22 -0400

    Add setters for fields and suppress warnings.
    References #1.

--- src/main/java/org/genspark/Phone.java
+++ src/main/java/org/genspark/Phone.java
@@ -1,5 +1,10 @@
 package org.genspark;
 
+@SuppressWarnings({"unused", "FieldCanBeLocal"})
 public class Phone {
     private String mob;
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
 }
--- src/main/java/org/genspark/Student.java
+++ src/main/java/org/genspark/Student.java
@@ -2,9 +2,30 @@ package org.genspark;
 
 import java.util.List;
 
+@SuppressWarnings({"unused", "FieldCanBeLocal"})
 public class Student {
     private int id;
     private String name;
     private List<Phone> ph;
     private Address add;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPh(List<Phone> ph) {
+        this.ph = ph;
+    }
+
+    public void setAdd(Address add) {
+        this.add = add;
+    }
 }
